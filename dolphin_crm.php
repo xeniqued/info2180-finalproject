bc005dd2d4ef961aaa50a4fe7acbb3caaae14a93
Shamar Malcolm <malco0253@gmail.com>
2024-12-06 21:41:56 -0500

    added user login functionality and adding a user

---
+++ dolphin_crm.php
@@ -0,0 +1,88 @@
+<?php
+
+//Starting session
+session_start();
+
+//Handling CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Accept");
+
+
+//Database credentials
+$db_host = 'localhost';
+$db_username = 'admin';
+$db_password = 'admin123';
+$dbname = 'dolphin_crm';
+
+
+//User credentials
+$firstname = GET['firstname'];
+$lastname = GET['lastname'];
+$password = GET['password'];
+$email = GET['email'];
+$role = GET['role'];
+
+
+
+//Connection to DB
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+
+//User login 
+if (isset($password) && isset($email)) {
+    $password = password_hash($password, md5);
+
+    $users = $conn->query("SELECT * FROM USERS WHERE email = '$email' AND password = '$password'");
+    $users = $users->fetch();
+    if($users.rowCount() > 0) {
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        $_SESSION['role'] = $role;
+    
+    }
+
+}
+
+//Adding a user
+
+$exp = "/[0-9][A-Z][a-z]{8,}/";
+if(isset($password) && isset($email) && isset($firstname) && isset($lastname) && preg_match($exp, $password)) {
+    $password = password_hash($password, md5);
+
+    $users = $conn->query("INSERT INTO USERS (firstname, lastname, email, password) VALUES('$firstname', '$lastname', '$email', '$password')");
+}
+else{
+    echo "Error adding user";
+}
+
+
+/*Viewing a User
+
+if($_SESSION['role'] == 'admin') {
+
+    $users = $conn->query("SELECT * FROM USERS");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    ?>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Created</th>
+      </tr>
+    <tbody>
+      <?php foreach ($results as $row): ?>
+        <tr>
+          <td><?=$row['name']; ?></td>
+          <td><?=$row['continent']; ?></td>
+          <td><?=$row['independence_year']; ?></td>
+          <td><?=$row['head_of_state']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php
+/*
+
+}
--- schema.sql
+++ schema.sql
@@ -1,34 +1,39 @@
-CREATE DATABASE dolpin_crm;
+CREATE DATABASE dolphin_crm;
+USE dolphin_crm;
 
-CREATE TABLE Users(
-    id int;
-    firstname varchar;
-    lastname varchar;
-    password varchar;
-    email varchar;
-)
+CREATE TABLE Users (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstname varchar(255),
+    lastname varchar(255),
+    password varchar(255),
+    email varchar(255),
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE Contacts(
-    id int AUTO_INCREMENT PRIMARY KEY;
-    title varchar;
-    firstname varchar;
-    lastname varchar;
-    email varchar;
-    telephone varchar;
-    company varchar;
-    type varchar;
-    assigned_to varchar;
-    created_by varchar;
-    created_at datetime;
-    updated_at datetime;
+CREATE TABLE Contacts (
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar(255),
+    firstname varchar(255),
+    lastname varchar(255),
+    email varchar(255),
+    telephone varchar(255),
+    company varchar(255),
+    type varchar(10),
+    assigned_to varchar(200),
+    created_by varchar(150),
+    created_at datetime(6),
+    updated_at datetime(6),
+    PRIMARY KEY (id)
+);
 
-)
+CREATE TABLE Notes (
+    id INT NOT NULL AUTO_INCREMENT,
+    contact_id int(150),
+    comment text(255),
+    created_by int(255),
+    created_at datetime(6),
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE Notes(
-    id int AUTO_INCREMENT PRIMARY KEY;
-    contact_id int;
-    comment text;
-    created_by int;
-    created_at datetime;
-    
-)
+INSERT INTO USERS (password, email) VALUES (md5('password123'), "adminproject2.com");
+
