5f4da6a5ca40cdf47841ab02b966a83a273cee4d
WatoDej <152533108+WatoDej@users.noreply.github.com>
2024-12-15 17:51:20 -0500

    Button itself works, doing email and password verification

--- loginscript.js
+++ loginscript.js
@@ -18,14 +18,38 @@ document.addEventListener("DOMContentLoaded", function () {
   function EmailVerification(){
 
   }
+
+  function PasswordVerification(){
+
+  }
   
   function Userlogin(){
-    /*if ((EmailVerification()== true) && (PasswordVerification()== true)){
-      window.location.assign("dashboard.html");
-    }*/
-    window.location.assign("dashboard.html");
+    const validity = `${EmailVerification()}+${PasswordVerification}`; //check for the return values of the password and email checks
+
+    switch (validity){
+      case "true+true":
+        window.location.assign("dashboard.html");//go to dashboard
+        break;
+      case "true+false":
+        alert("Password is incorrect. Please try again.");
+        window.location.assign("loginindex.html");//to ensure reload of the login page once we press ok
+        break;
+      case "false+true":
+        alert("Email is incorrect. Please try again.");
+        window.location.assign("loginindex.html");
+        break;
+      case "false+false":
+        alert("Email and Password incorrect. Please try again.");
+        window.location.assign("loginindex.html");
+        break;
+      default:
+        alert("Something went wrong. Please try again.");
+        window.location.assign("loginindex.html");
+        break;
 
 
-   // alert("Password is incorrect. Please try again");
+    }
+
+    //alert("Password is incorrect. Please try again");
   }
 });
