b10eb5c706f8ea0b046fffd82651918b0f211b45
WatoDej <152533108+WatoDej@users.noreply.github.com>
2024-12-15 14:56:18 -0500

    started login button functionality, removed header.php as was not necessary

--- loginscript.js
+++ loginscript.js
@@ -1,9 +1,17 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
+  //masking the password
   const textarea = document.getElementById('password');
   textarea.addEventListener('input', (event) => {
   const currentLength = event.target.value.length;
   textarea.value = '*'.repeat(currentLength);
 });
+  //login button functionality
+  const login = document.getElementById('loginbutton');
+  login.addEventListener('click', Userlogin);
+
+  function Userlogin(){
+    alert("Password is incorrect. Please try again");
+  }
 });
